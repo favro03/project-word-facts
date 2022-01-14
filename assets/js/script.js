30a952afaf85e3e0d8dd47fe39b4ea0f19285315
Christian.Sopoco <cjsopoco@gmail.com>
2022-01-14 12:36:38 -0800

    history display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-var populateSavedWords = function() {
-       // Get array from local storage
-       let wordsLocalStorage = JSON.parse(localStorage.getItem("savedWords"));
-
-       // Word exist or not. 0 = not, 1 = yes
-       let wordExist = 0;
-         
-       if (wordLocalStorage === null) {
-           // It does not exist, therefore, no items to add to saved cities
-           //console.log("No items to add");  
-       } else { // we will populate the saved words
-
-       $(".list-group-item").remove(); // Remove all list items from the document with jquery
-           
-        for (i=0; i< wordsLocalStorage.length;i++) {
-
-            // Populate the words as anchors and add necessary attributes and classes.
-            let wordNameEl = document.createElement("a")
-            let splitwordText = "";
-            wordNameEl.setAttribute("href", "#")
-            wordNameEl.setAttribute("data-word", wordsLocalStorage[i]);
-            wordNameEl.setAttribute("id", wordsLocalStorage[i]);
-            wordNameEl.setAttribute("role", "button");
-            wordNameEl.classList = "list-group-item list-group-item-action list-group-item-primary";
-            wordNameEl.textContent = wordsLocalStorage[i];
-            //wordsListContainerEl.appendChild(wordNameEl);
-            // dynContainer
-            wordsContainerEl.appendChild(wordNameEl);
-        };
-          // alert("All saved words have been populated");
-       };
-};
-=======
 var userFormEl = document.querySelector("#user-form");
 var wordInputEl = document.querySelector("#word");
 
@@ -42,5 +8,38 @@ var formSubmitHandler = function(event){
 
     console.log(word)
 }
+
+var populateSavedWords = function() {
+    // Get array from local storage
+    let wordsLocalStorage = JSON.parse(localStorage.getItem("savedWords"));
+
+    // Word exist or not. 0 = not, 1 = yes
+    let wordExist = 0;
+      
+    if (wordLocalStorage === null) {
+        // It does not exist, therefore, no items to add to saved cities
+        //console.log("No items to add");  
+    } else { // we will populate the saved words
+
+    $(".list-group-item").remove(); // Remove all list items from the document with jquery
+        
+     for (i=0; i< wordsLocalStorage.length;i++) {
+
+         // Populate the words as anchors and add necessary attributes and classes.
+         let wordNameEl = document.createElement("a")
+         let splitwordText = "";
+         wordNameEl.setAttribute("href", "#")
+         wordNameEl.setAttribute("data-word", wordsLocalStorage[i]);
+         wordNameEl.setAttribute("id", wordsLocalStorage[i]);
+         wordNameEl.setAttribute("role", "button");
+         wordNameEl.classList = "list-group-item list-group-item-action list-group-item-primary";
+         wordNameEl.textContent = wordsLocalStorage[i];
+         //wordsListContainerEl.appendChild(wordNameEl);
+         // dynContainer
+         wordsContainerEl.appendChild(wordNameEl);
+     };
+       // alert("All saved words have been populated");
+    };
+};
+
 userFormEl.addEventListener("submit", formSubmitHandler);
->>>>>>> d0cc12e24b07157fd35593632159187f7b8388d0
