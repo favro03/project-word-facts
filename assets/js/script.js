61e139062402ddc77808f562a2a3fa27f85d7eef
Christian.Sopoco <cjsopoco@gmail.com>
2022-01-14 13:11:20 -0800

    update historydisplay

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,82 +1,64 @@
 var userFormEl = document.querySelector("#user-form");
 var wordInputEl = document.querySelector("#word");
 
+var storageArr = [];
+
+var locationStorage = function(word){
+    storageArr = JSON.parse(window.localStorage.getItem("word")) || [];
+    var value = word;
+    if(storageArr.indexOf(value)== -1){
+        storageArr.push(value);
+    }
+    
+    localStorage.setItem("word", JSON.stringify(storageArr));
+};
+
 var formSubmitHandler = function(event){
     event.preventDefault();
-    
     var word = wordInputEl.value.trim();
-
+    locationStorage(word);
+    wordInputEl.value="";
     console.log(word)
 }
-<<<<<<< HEAD
-
-var populateSavedWords = function() {
-    // Get array from local storage
-    let wordsLocalStorage = JSON.parse(localStorage.getItem("savedWords"));
-
-    // Word exist or not. 0 = not, 1 = yes
-    let wordExist = 0;
-      
-    if (wordLocalStorage === null) {
-        // It does not exist, therefore, no items to add to saved cities
-        //console.log("No items to add");  
-    } else { // we will populate the saved words
-
-    $(".list-group-item").remove(); // Remove all list items from the document with jquery
-        
-     for (i=0; i< wordsLocalStorage.length;i++) {
-
-         // Populate the words as anchors and add necessary attributes and classes.
-         let wordNameEl = document.createElement("a")
-         let splitwordText = "";
-         wordNameEl.setAttribute("href", "#")
-         wordNameEl.setAttribute("data-word", wordsLocalStorage[i]);
-         wordNameEl.setAttribute("id", wordsLocalStorage[i]);
-         wordNameEl.setAttribute("role", "button");
-         wordNameEl.classList = "list-group-item list-group-item-action list-group-item-primary";
-         wordNameEl.textContent = wordsLocalStorage[i];
-         //wordsListContainerEl.appendChild(wordNameEl);
-         // dynContainer
-         wordsContainerEl.appendChild(wordNameEl);
-     };
-       // alert("All saved words have been populated");
-    };
-};
-
-userFormEl.addEventListener("submit", formSubmitHandler);
-=======
 userFormEl.addEventListener("submit", formSubmitHandler);
 
-var populateSavedWords = function() {
-       // Get array from local storage
-       let wordsLocalStorage = JSON.parse(localStorage.getItem("savedWords"));
+var wordbox = [];
+//Will save the text value of the search and save it to an array and storage
+$('.search').on("click", function (event) {
+	event.preventDefault();
+	word = $(this).parent('.btnPar').siblings('.textVal').val().trim();
+	if (word === "") {
+		return;
+	};
+	wordbox.push(word);
 
-       // Word exist or not. 0 = not, 1 = yes
-       let wordExist = 0;
-         
-       if (wordLocalStorage === null) {
-           // It does not exist, therefore, no items to add to saved cities
-           //console.log("No items to add");  
-       } else { // we will populate the saved words
+	localStorage.setItem('word', JSON.stringify(wordbox));
 
-       $(".list-group-item").remove(); // Remove all list items from the document with jquery
-           
-        for (i=0; i< wordsLocalStorage.length;i++) {
+});
 
-            // Populate the words as anchors and add necessary attributes and classes.
-            let wordNameEl = document.createElement("a")
-            let splitwordText = "";
-            wordNameEl.setAttribute("href", "#")
-            wordNameEl.setAttribute("data-word", wordsLocalStorage[i]);
-            wordNameEl.setAttribute("id", wordsLocalStorage[i]);
-            wordNameEl.setAttribute("role", "button");
-            wordNameEl.classList = "list-group-item list-group-item-action list-group-item-primary";
-            wordNameEl.textContent = wordsLocalStorage[i];
-            //wordsListContainerEl.appendChild(wordNameEl);
-            // dynContainer
-            wordsContainerEl.appendChild(wordNameEl);
-        };
-          // alert("All saved words have been populated");
-       };
+//Will create buttons based on search history 
+var contHistEl = $('.wordbox');
+function getHistory() {
+	contHistEl.empty();
+
+	for (let i = 0; i < wordbox.length; i++) {
+
+		var rowEl = $('<row>');
+		var btnEl = $('<button>').text(`${wordbox[i]}`)
+
+		rowEl.addClass('row histBtnRow');
+		btnEl.addClass('btn btn-outline-secondary histBtn');
+		btnEl.attr('type', 'button');
+
+		contHistEl.prepend(rowEl);
+		rowEl.append(btnEl);
+	} if (!word) {
+		return;
+	}
+	//Allows the buttons to start a search as well
+	$('.histBtn').on("click", function (event) {
+		event.preventDefault();
+		word = $(this).text();
+	});
 };
->>>>>>> 66b02ea2efba4b07b1d93f1fbbdf0f1d8df64148
+
