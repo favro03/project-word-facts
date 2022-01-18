940c39ba5a4dbbcde5e62adcbac0c8abd36fdd5f
Pao Her <p.her007@hotmail.com>
2022-01-17 20:39:01 -0600

    adding fetch quote api call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 var userFormEl = document.querySelector("#user-form");
 var wordInputEl = document.querySelector("#word");
 
+
+
 var storageArr = [];
 
 var locationStorage = function(word){
@@ -16,8 +18,58 @@ var locationStorage = function(word){
 var formSubmitHandler = function(event){
     event.preventDefault();
     var word = wordInputEl.value.trim();
+    fetchAPI(word)
+    
     locationStorage(word);
     wordInputEl.value="";
-    console.log(word)
+   
+
+    
 }
 userFormEl.addEventListener("submit", formSubmitHandler);
+
+
+
+
+
+function fetchAPI(word){
+
+    fetch("https://type.fit/api/quotes")
+    .then(response => response.json())
+    .then(function(data){
+        var arr = [];
+
+            for(var count = 0; count < data.length; count++){
+
+                var flag = false;
+
+                var str = data[count].text;
+    
+                    arr = str.split(" ");
+                for(var i = 0; i < arr.length; i++){
+                    if(arr[i] === word){
+                        
+                       $(".quotes").text(str)
+                        flag = true;
+                        
+                    }
+                    
+                }
+
+                if(flag){
+                    break;
+                }
+
+                if(count == (data.length -1)){
+
+                    $(".quotes").text('No Quote')
+                }
+
+             
+    
+            }
+
+
+    })
+}
+
